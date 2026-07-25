ecf0aca91a3ddb1d1b2985e47c12a1ff89f05873
Louis Stockton <louisstockton1@gmail.com>
2026-07-25 16:37:53 +0100

    Register planning models with the Django admin - ServiceRoleInLine on Service, RolePieceInLine on ServiceRole (nested inlines aren't supported, so roles/pieces are managed one level down rather than all on one page) - display_status surfaces Service.status (a derived @property) in the admin list view, since properties can't be sorted/filtered like real fields

--- planning/admin.py
+++ planning/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import RolePiece, Service, ServiceRole, Term
+
+
+class ServiceRoleInline(admin.TabularInline):
+    model = ServiceRole
+    extra = 1
+
+
+class RolePieceInline(admin.TabularInline):
+    model = RolePiece
+    extra = 1
+
+
+@admin.register(Term)
+class TermAdmin(admin.ModelAdmin):
+    list_display = ["name", "start_date", "end_date"]  # noqa
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ["service_type", "date", "term", "occasion", "display_status"]  # noqa
+    list_filter = ["term", "service_type"]  # noqa
+    inlines = [ServiceRoleInline]  # noqa
+
+    def display_status(self, obj):
+        return obj.status
+
+    display_status.short_description = "Status"
+
+
+@admin.register(ServiceRole)
+class ServiceRoleAdmin(admin.ModelAdmin):
+    list_display = ["role_name", "service", "is_not_applicable"]  # noqa
+    inlines = [RolePieceInline]  # noqa
