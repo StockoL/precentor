e2670445ae42af2bbc7e2633cb4066c796d6b706
Louis Stockton <louisstockton1@gmail.com>
2026-07-26 09:13:31 +0100

    Added Term.get_absolute_url() for Create/Update redirect (forgotten staging from two commits ago)

--- planning/models.py
+++ planning/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Term(models.Model):
@@ -11,6 +12,9 @@ class Term(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("planning:term_detail", kwargs={"pk": self.pk})
 
 
 class Service(models.Model):
