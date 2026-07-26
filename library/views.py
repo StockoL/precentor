556a61cb9f399d6e038154d951c6570adf556c13
Louis Stockton <louisstockton1@gmail.com>
2026-07-26 20:44:30 +0100

    fix: correct the misplaced ProtectedError import - ProtectedError was incorrectly listed as an import from library.models, but is a Django-provided exception living in django.db.models

--- library/views.py
+++ library/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import ProtectedError
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import (
@@ -10,7 +11,7 @@ from django.views.generic import (
     UpdateView,
 )
 
-from .models import ProtectedError, Score
+from .models import Score
 
 
 class ScoreListView(LoginRequiredMixin, ListView):
